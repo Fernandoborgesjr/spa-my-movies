0b7ee341b2b947772835146ec2975c36dc55aaeb
Fernando Borges <fernandomborges96@gmail.com>
2024-10-05 15:33:57 -0300

    feat(components): add MainMenu

---
+++ src/components/MainMenu/index.jsx
@@ -0,0 +1,10 @@
+import { MainNavContainer } from '../styled/MainNavContainer'
+
+export const MainMenu = () => (
+  <MainNavContainer>
+    <ul>
+      <li>Início</li>
+      <li>Sobre</li>
+    </ul>
+  </MainNavContainer>
+)
