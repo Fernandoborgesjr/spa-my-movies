20abaa741a42887951b5358b059eb9b11e71d69b
Fernando Borges <fernandomborges96@gmail.com>
2024-10-05 15:34:18 -0300

    feat(components): add Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import { HeaderContainer } from '../styled/HeaderContainer'
+import { MainMenu } from '../MainMenu'
+
+export const Header = () => (
+  <HeaderContainer>
+    <h1>Meu Filmes</h1>
+
+    <MainMenu />
+  </HeaderContainer>
+)
