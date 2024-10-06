9e95e7b1f002441aa04f7405362ae0e263d8d43a
Fernando Borges <fernandomborges96@gmail.com>
2024-10-06 19:45:17 -0300

    chore(env): rename REACT_APP_TMDB_BASE_URL to REACT_APP_TMDB_API_URL

--- src/infra/MoviesService.js
+++ src/infra/MoviesService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export class MoviesService {
 
-  static #options = { baseURL: process.env.REACT_APP_TMDB_BASE_URL, params: {api_key: process.env.REACT_APP_TMDB_API_KEY} }
+  static #options = { baseURL: process.env.REACT_APP_TMDB_API_URL, params: {api_key: process.env.REACT_APP_TMDB_API_KEY} }
 
   static getPopular() {
     return axios.get('movie/popular', this.#options)
