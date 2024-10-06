1c39cd18fcc75b3f19ae8afa76476da256714a80
Fernando Borges <fernandomborges96@gmail.com>
2024-10-06 19:24:39 -0300

    feat(infra, movies-service): add getMovieById

--- src/infra/MoviesService.js
+++ src/infra/MoviesService.js
@@ -1,7 +1,14 @@
 import axios from 'axios'
 
 export class MoviesService {
-  static getPopularMovies() {
-    return axios.get('movie/popular', { baseURL: process.env.REACT_APP_TMDB_BASE_URL, params: {api_key: process.env.REACT_APP_TMDB_API_KEY} })
+
+  static #options = { baseURL: process.env.REACT_APP_TMDB_BASE_URL, params: {api_key: process.env.REACT_APP_TMDB_API_KEY} }
+
+  static getPopular() {
+    return axios.get('movie/popular', this.#options)
+  }
+
+  static getMovieById(id){
+    return axios.get(`movie/${id}`, this.#options)
   }
 }
